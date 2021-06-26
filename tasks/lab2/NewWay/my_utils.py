0780b5a6a7e8be11a0a188c68bc9c2bdd28dc1a7
Arsenii Margasov <margasovarsenij@gmail.com>
2021-06-26 16:18:49 +0300

    Added lab2

---
+++ tasks/lab2/NewWay/my_utils.py
@@ -0,0 +1,459 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from torchtext.legacy.data import Field, BucketIterator, TabularDataset
+from torchtext.data.metrics import bleu_score
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+import spacy
+import numpy as np
+from tqdm.notebook import tqdm
+
+import random
+import math
+import time
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def train_transformer(model, iterator, optimizer, criterion, clip):
+    model.train()
+    
+    epoch_loss = 0
+    
+    for i, batch in enumerate(iterator):
+        
+        src = batch.src
+        trg = batch.trg
+        
+        optimizer.zero_grad()
+        
+        output, _ = model(src, trg[:,:-1])
+                
+        #output = [batch size, trg len - 1, output dim]
+        #trg = [batch size, trg len]
+            
+        output_dim = output.shape[-1]
+            
+        output = output.contiguous().view(-1, output_dim)
+        trg = trg[:,1:].contiguous().view(-1)
+                
+        #output = [batch size * trg len - 1, output dim]
+        #trg = [batch size * trg len - 1]
+            
+        loss = criterion(output, trg)
+        
+        loss.backward()
+        
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+
+
+def evaluate_transformer(model, iterator, criterion):
+    model.eval()
+    
+    epoch_loss = 0
+    
+    with torch.no_grad():
+    
+        for i, batch in enumerate(iterator):
+
+            src = batch.src
+            trg = batch.trg
+
+            output, _ = model(src, trg[:,:-1])
+            
+            #output = [batch size, trg len - 1, output dim]
+            #trg = [batch size, trg len]
+            
+            output_dim = output.shape[-1]
+            
+            output = output.contiguous().view(-1, output_dim)
+            trg = trg[:,1:].contiguous().view(-1)
+            
+            #output = [batch size * trg len - 1, output dim]
+            #trg = [batch size * trg len - 1]
+            
+            loss = criterion(output, trg)
+
+            epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+
+
+def train_cnn(model, iterator, optimizer, criterion, clip):
+    model.train()
+    
+    epoch_loss = 0
+    
+    for i, batch in enumerate(iterator):
+        
+        src = batch.src
+        trg = batch.trg
+        
+        optimizer.zero_grad()
+        
+        output, _ = model(src, trg[:,:-1])
+        
+        #output = [batch size, trg len - 1, output dim]
+        #trg = [batch size, trg len]
+        
+        output_dim = output.shape[-1]
+        
+        output = output.contiguous().view(-1, output_dim)
+        trg = trg[:,1:].contiguous().view(-1)
+        
+        #output = [batch size * trg len - 1, output dim]
+        #trg = [batch size * trg len - 1]
+        
+        loss = criterion(output, trg)
+        
+        loss.backward()
+        
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+
+
+def evaluate_cnn(model, iterator, criterion):
+    model.eval()
+    
+    epoch_loss = 0
+    
+    with torch.no_grad():
+    
+        for i, batch in enumerate(iterator):
+
+            src = batch.src
+            trg = batch.trg
+
+            output, _ = model(src, trg[:,:-1])
+        
+            #output = [batch size, trg len - 1, output dim]
+            #trg = [batch size, trg len]
+
+            output_dim = output.shape[-1]
+            
+            output = output.contiguous().view(-1, output_dim)
+            trg = trg[:,1:].contiguous().view(-1)
+
+            #output = [batch size * trg len - 1, output dim]
+            #trg = [batch size * trg len - 1]
+            
+            loss = criterion(output, trg)
+
+            epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
+def transformer_translate_sentence(sentence, src_field, trg_field, model, device, max_len = 50, greedy = True):
+    model.eval()
+        
+    if isinstance(sentence, str):
+        nlp = spacy.load('en_core_web_sm')
+        tokens = [token.text.lower() for token in nlp(sentence)]
+    else:
+        tokens = [token.lower() for token in sentence]
+
+    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+        
+    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
+
+    src_tensor = torch.LongTensor(src_indexes).unsqueeze(0).to(device)
+    
+    src_mask = model.make_src_mask(src_tensor)
+    
+    with torch.no_grad():
+        enc_src = model.encoder(src_tensor, src_mask)
+
+    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+
+    for i in range(max_len):
+
+        trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(device)
+
+        trg_mask = model.make_trg_mask(trg_tensor)
+        
+        with torch.no_grad():
+            output, attention = model.decoder(trg_tensor, enc_src, trg_mask, src_mask)
+        if greedy:
+            pred_token = output.argmax(2)[:,-1].item()
+        else:
+            probs = F.softmax(output, dim=-1)[0, 0, :]
+            pred_token = torch.multinomial(probs, 1)
+        
+        trg_indexes.append(pred_token)
+
+        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
+            break
+    
+    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+    
+    return trg_tokens[1:], attention
+
+
+def transformer_display_attention(sentence, translation, attention, n_heads = 8, n_rows = 4, n_cols = 2):
+    assert n_rows * n_cols == n_heads
+    
+    fig = plt.figure(figsize=(15,25))
+    
+    for i in range(n_heads):
+        
+        ax = fig.add_subplot(n_rows, n_cols, i+1)
+        
+        _attention = attention.squeeze(0)[i].cpu().detach().numpy()
+
+        cax = ax.matshow(_attention, cmap='bone')
+
+        ax.tick_params(labelsize=12)
+        ax.set_xticklabels(['']+['<sos>']+[t.lower() for t in sentence]+['<eos>'], 
+                           rotation=45)
+        ax.set_yticklabels(['']+translation)
+
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+    plt.close()
+    
+    
+def cnn_translate_sentence(sentence, src_field, trg_field, model, device, max_len = 50):
+    model.eval()
+        
+    if isinstance(sentence, str):
+        nlp = spacy.load('de_core_news_sm')
+        tokens = [token.text.lower() for token in nlp(sentence)]
+    else:
+        tokens = [token.lower() for token in sentence]
+
+    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+        
+    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
+
+    src_tensor = torch.LongTensor(src_indexes).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        encoder_conved, encoder_combined = model.encoder(src_tensor)
+
+    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+
+    for i in range(max_len):
+
+        trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(device)
+
+        with torch.no_grad():
+            output, attention = model.decoder(trg_tensor, encoder_conved, encoder_combined)
+        
+        pred_token = output.argmax(2)[:,-1].item()
+        
+        trg_indexes.append(pred_token)
+
+        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
+            break
+    
+    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+    
+    return trg_tokens[1:], attention
+    
+    
+def cnn_display_attention(sentence, translation, attention):
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111)
+        
+    attention = attention.squeeze(0).cpu().detach().numpy()
+    
+    cax = ax.matshow(attention, cmap='bone')
+   
+    ax.tick_params(labelsize=15)
+    ax.set_xticklabels(['']+['<sos>']+[t.lower() for t in sentence]+['<eos>'], 
+                       rotation=45)
+    ax.set_yticklabels(['']+translation)
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+    plt.close()
+    
+    
+def train_gru(model, iterator, optimizer, criterion, clip):
+    model.train()
+    
+    epoch_loss = 0
+    
+    for i, batch in enumerate(iterator):
+        
+        src, src_len = batch.src
+        trg = batch.trg
+        
+        optimizer.zero_grad()
+        
+        output = model(src, src_len, trg)
+        
+        #trg = [trg len, batch size]
+        #output = [trg len, batch size, output dim]
+        
+        output_dim = output.shape[-1]
+        
+        output = output[1:].view(-1, output_dim)
+        trg = trg[1:].view(-1)
+        
+        #trg = [(trg len - 1) * batch size]
+        #output = [(trg len - 1) * batch size, output dim]
+        
+        loss = criterion(output, trg)
+        
+        loss.backward()
+        
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+
+
+def evaluate_gru(model, iterator, criterion):
+    model.eval()
+    
+    epoch_loss = 0
+    
+    with torch.no_grad():
+    
+        for i, batch in enumerate(iterator):
+
+            src, src_len = batch.src
+            trg = batch.trg
+
+            output = model(src, src_len, trg, 0) #turn off teacher forcing
+            
+            #trg = [trg len, batch size]
+            #output = [trg len, batch size, output dim]
+
+            output_dim = output.shape[-1]
+            
+            output = output[1:].view(-1, output_dim)
+            trg = trg[1:].view(-1)
+
+            #trg = [(trg len - 1) * batch size]
+            #output = [(trg len - 1) * batch size, output dim]
+
+            loss = criterion(output, trg)
+
+            epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+    
+
+def gru_translate_sentence(sentence, src_field, trg_field, model, device, max_len = 50):
+    model.eval()
+        
+    if isinstance(sentence, str):
+        nlp = spacy.load('ru')
+        tokens = [token.text.lower() for token in nlp(sentence)]
+    else:
+        tokens = [token.lower() for token in sentence]
+
+    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+        
+    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
+    
+    src_tensor = torch.LongTensor(src_indexes).unsqueeze(1).to(device)
+
+    src_len = torch.LongTensor([len(src_indexes)])
+    
+    with torch.no_grad():
+        encoder_outputs, hidden = model.encoder(src_tensor, src_len)
+
+    mask = model.create_mask(src_tensor)
+        
+    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+
+    attentions = torch.zeros(max_len, 1, len(src_indexes)).to(device)
+    
+    for i in range(max_len):
+
+        trg_tensor = torch.LongTensor([trg_indexes[-1]]).to(device)
+                
+        with torch.no_grad():
+            output, hidden, attention = model.decoder(trg_tensor, hidden, encoder_outputs, mask)
+
+        attentions[i] = attention
+            
+        pred_token = output.argmax(1).item()
+        
+        trg_indexes.append(pred_token)
+
+        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
+            break
+    
+    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+    
+    return trg_tokens[1:], attentions[:len(trg_tokens)-1]    
+
+
+def gru_display_attention(sentence, translation, attention):
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111)
+    
+    attention = attention.squeeze(1).cpu().detach().numpy()
+    
+    cax = ax.matshow(attention, cmap='bone')
+   
+    ax.tick_params(labelsize=15)
+    
+    x_ticks = [''] + ['<sos>'] + [t.lower() for t in sentence] + ['<eos>']
+    y_ticks = [''] + translation
+     
+    ax.set_xticklabels(x_ticks, rotation=45)
+    ax.set_yticklabels(y_ticks)
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+    plt.close()
+
+    
+def calculate_bleu(data, src_field, trg_field, model, device, func=translate_sentence, max_len = 50):
+    trgs = []
+    pred_trgs = []
+    
+    for datum in data:
+        
+        src = vars(datum)['src']
+        trg = vars(datum)['trg']
+        
+        pred_trg, _ = func(src, src_field, trg_field, model, device, max_len)
+        
+        #cut off <eos> token
+        pred_trg = pred_trg[:-1]
+        
+        pred_trgs.append(pred_trg)
+        trgs.append([trg])
+        
+    return bleu_score(pred_trgs, trgs)
